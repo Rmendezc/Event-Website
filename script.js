bac4e20cadb1cd10991fe6139473f65e52520bec
Rudy Mendez <rudymendez32@gmail.com>
2024-03-23 19:31:38 -0700

    Here is the finalized website and updated my jquery components

--- script.js
+++ script.js
@@ -3,40 +3,51 @@ $(document).ready(function(){
       dots: true,
       arrows: true,
       autoplay: true,
-      autoplaySpeed: 5000
+      autoplaySpeed: 5000,
+      adaptiveHeight: true // Set adaptiveHeight to true to adjust the height of the slider content
     });
   });
 
 
-function ticketInfo (){
+
+  function ticketInfo() {
     var button = document.getElementById('entBtn');
     button.addEventListener("click", save, false);
     display();
 }
-function save(){
+
+function save() {
     var ticketName = document.getElementById('ticketName').value;
     var ticketNumber = document.getElementById('ticketNumber').value;
-    sessionStorage.setItem(ticketName,ticketNumber);
+    // Use a fixed key or generate a unique key for sessionStorage
+    sessionStorage.setItem(ticketName, ticketNumber);
     display();
-    ticketName.value=" ";
-    ticketNumber.value=" ";
-}
-function display(){
-    var displayE = document.getElementById('displayE');
-    displayE.innerHTML=" ";
-    for(var i = 0; i< sessionStorage.length; i++){
-        var x = sessionStorage.ticketName(i);
-        var z = sessionStorage.getItem(x);
-        displayE.innerHTML += x + "  " + z + "  ";
-    }
-}
-function clear(){
-    var button = document.getElementById('clBtn');
-    button.addEventListener("click", save, false);
-    display();
+    document.getElementById('ticketName').value = ""; // Clear input value
+    document.getElementById('ticketNumber').value = ""; // Clear input value
 }
 
-window.addEventListener("load", ticketInfo, false)
+function display() {
+    var displayE = document.getElementById('displayE');
+    displayE.innerHTML = ""; // Clear previous content
+    for (var i = 0; i < sessionStorage.length; i++) {
+        var key = sessionStorage.key(i); // Get the key at index i
+        var value = sessionStorage.getItem(key); // Get the corresponding value
+        displayE.innerHTML += key + "  " + value + "<br>"; // Display key-value pair
+    }
+}
+
+// Function to clear sessionStorage
+function clearStorage() {
+    sessionStorage.clear();
+    display(); // Update display after clearing
+}
+
+window.addEventListener("load", function() {
+    ticketInfo(); // Call ticketInfo function when the window loads
+    var clearBtn = document.getElementById('clBtn');
+    clearBtn.addEventListener("click", clearStorage, false); // Add event listener for clear button
+}, false);
+
 
 
 $.ajax({
